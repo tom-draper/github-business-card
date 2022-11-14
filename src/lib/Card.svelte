b8a19a2cd616281e0c125f46c19252f1724d45a8
Tom Draper <tomjdraper1@gmail.com>
2022-11-14 20:06:26 +0000

    Init

---
+++ src/lib/Card.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export let data: any;
+</script>
+
+<div id="card">
+    <div class="name">
+        {data.user.name}
+    </div>
+    <div class="github-url">
+        <span class="faint">github.com/</span>{data.user.login}
+    </div>
+    <div class="stats">
+        <div class="stars">{data.stats.stars} stars</div>
+        <div class="forks">{data.stats.forks} forks</div>
+    </div>
+</div>
+
+<style>
+#card {
+    width: 400px;
+    height: 200px;
+    background: white;
+    color: black;
+    padding: 50px;
+    text-align: left;
+    border-radius: 3px;
+    margin: 100px 0;
+}
+.name {
+    font-weight: 700;
+    font-size: 32px;
+    margin: 10px 0;
+}
+.github-url {
+    font-size: 18px;
+}
+.faint {
+    opacity: 0.5;
+}
+</style>
