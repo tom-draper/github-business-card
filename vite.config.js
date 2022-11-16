34ac0febe0d4842996ca47490c5b3715b1711c98
Tom Draper <tomjdraper1@gmail.com>
2022-11-16 12:01:23 +0000

    Build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-business-card/',
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    outDir: 'docs'
+  }
 })
