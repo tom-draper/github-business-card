384da83c96f023a983993072b9f6bb6999745308
Tom Draper <tomjdraper1@gmail.com>
2022-11-16 11:33:23 +0000

    Remove unused files

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -62,7 +62,18 @@
     <div class="details">
       <div class="location-date">
         <div class="location">
-          <svg class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path></svg>{data.user.location}
+          <svg
+            class="octicon octicon-location"
+            viewBox="0 0 12 12"
+            version="1.1"
+            width="12"
+            height="12"
+            aria-hidden="true"
+            ><path
+              fill-rule="evenodd"
+              d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
+            /></svg
+          >{data.user.location}
         </div>
       </div>
 
@@ -76,7 +87,10 @@
       </div>
     </div>
     <div class="since">
-      Since {new Date(data.user.created_at).toLocaleDateString('en-us', { year:"numeric", month:"long"})}
+      Since {new Date(data.user.created_at).toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "long",
+      })}
     </div>
   </div>
   <div class="languages">
